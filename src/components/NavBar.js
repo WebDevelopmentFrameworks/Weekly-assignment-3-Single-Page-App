a5cfa6f4334b70143ad614488cb6618acbac0884
WellyBelly <89990168+WellyBelly@users.noreply.github.com>
2022-09-20 10:37:42 +0300

    Updated Navbar.js to make it more responive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,24 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-0">
-      <div>
-        <ul className="nav">
-          <li className="nav-item my-auto">
-            <Link className="navbar-brand text-white ms-3 my-auto fs-4" aria-current="page" to="/">SinglePA</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link text-white-50" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link text-white-50" to="/about">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link text-white-50" to="/peekaboo">Peek a Boo</Link>
-          </li>
-        </ul>
+      <div className="container-fluid">
+        <Link className="navbar-brand text-white ms-3 my-auto fs-4" aria-current="page" to="/">SinglePA</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="nav">
+            <li className="nav-item">
+              <Link className="nav-link text-white-50" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white-50" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white-50" to="/peekaboo">Peek a Boo</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   ) 
